c8aa412a3ee2b945a6f18d4b08094e99dca273f6
UlissesSilva08 <ulisses08@hotmail.com>
2019-09-14 05:23:13 +0900

    reposicionando as inputs e adição da função da camera

--- www/components/action.js
+++ www/components/action.js
@@ -1,4 +1,4 @@
- function camera(){
+$(document).on("click","#camera",function(){
    navigator.camera.getPicture(onSucess, onFail,{
    quality: 100,
    destinationType: Camera.DestinationType.FILE_URI,
@@ -14,4 +14,4 @@
    function onFail(message){
      navigator.notification.alert("Failed because: " + message);
    }
-}
+});
