892182298bd0c5d1556e037523fbdc5e79c9e7bc
UlissesSilva08 <ulisses08@hotmail.com>
2019-09-18 02:39:58 +0900

    Sombreamento nos textos, estilização do botão e função das inputs

--- www/components/action.js
+++ www/components/action.js
@@ -15,3 +15,20 @@ $(document).on("click","#camera",function(){
      navigator.notification.alert("Failed because: " + message);
    }
 });
+
+$(document).on("change","#myRange",function(){
+  $("#image").css("filter","opacity("+$(this).val()+"%)");
+})
+
+$(document).on("change","#myRange1",function(){
+  $("#image").css("filter","saturate("+$(this).val()+"%)");
+})
+
+$(document).on("change","#myRange2",function(){
+  $("#image").css("filter","grayscale("+$(this).val()+"%)");
+})
+
+$(document).on("change","#myRange3",function(){
+  $("#image").css("filter","blur("+$(this).val()+"px)");
+})
+
